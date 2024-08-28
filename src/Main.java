d5c7a628b0fa89317bd5405ca82da0656faeae90
Dastanbek Keneshbekov <dastan.keneshbekov.kk@gmail.com>
2024-08-29 02:07:33 +0600

    Созал комментрарий для примера

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+
+
+        int x = 7;
+//        x = x & 25;
+//        x = x & 5;
+//        x = x & 3;
+//        x = x & 12;
+        x = x | 1;
+        System.out.println(x);
+
+
+
+
+    }
+
+//    public static String getPrediction() throws InterruptedException {
+//        //напишите тут ваш код
+//
+//        for (int i = 0; i <= 10; i++) {
+//            System.out.println(i);
+//            Thread.sleep(0, 130000);
+//        }
+//
+//        return "";
+//    }
+
+}
+
+
